4b0e241619cfeddd2184147ef63e203c8923307a
Administrator <Administrator@LAPTOP-OIF4RB1I>
2020-06-01 07:13:09 +0300

    first commit

--- special/settings.py
+++ special/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0=4!f^*+b+*#^kn(ogj_a&t%6sr)ailioxk17a5do+5phqwc&$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'sirat-spc.herokuapp.com']
 
